282e94ac2774d8b92996b100ea58d4e678477c9d
Fabio Cutrupi <fabio.cutrupi@polomanettiporciatti.edu.it>
2024-12-19 12:05:53 +0100

    Mi sento fortunato

--- script.js
+++ script.js
@@ -65,3 +65,44 @@ document.addEventListener("DOMContentLoaded", async () => {
   // Carica i prodotti
   await fetchProducts();
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const randomAPIUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+    // Funzione per caricare i prodotti
+    async function fetchProductsrandom() {
+        try {
+            const response = await fetch(randomAPIUrl);
+            const data = await response.json();
+            const products = data.drinks;
+            renderProductss(products);
+        } catch (error) {
+            console.error("Errore nel caricamento dei prodotti", error);
+        }
+    }
+
+    // Funzione per renderizzare i prodotti
+    function renderProductss(products) {
+        const productList = document.getElementById("productLists");
+        productList.innerHTML = ""; // Pulisce la lista esistente
+
+        products.forEach(product => {
+            const productDiv = document.createElement("div");
+            productDiv.classList.add("product");
+            productDiv.setAttribute("data-name", product.strDrink.toLowerCase());
+            productDiv.innerHTML = `
+                <img src="${product.strDrinkThumb}" alt="${product.strDrink}">
+                <p>${product.strDrink.toUpperCase()}</p>
+            `;
+            productList.appendChild(productDiv);
+        });
+    }
+
+    // Aggiungi evento al pulsante per chiamare l'API
+    const randomButton = document.getElementById("randomButton");
+    randomButton.addEventListener("click", async () => {
+        await fetchProductsrandom();
+    });
+    fetchProducts();
+});
+

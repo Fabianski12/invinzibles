ad36812ef2c8ed4325f8d3c73c415362bf48ef72
Fabio Cutrupi <fabio.cutrupi@polomanettiporciatti.edu.it>
2025-02-05 13:04:07 +0100

    dettagli drink inseriti

--- menu.js
+++ menu.js
@@ -1,49 +1,50 @@
 document.getElementById("showMoreButton").addEventListener("click", () => {
     const cocktailApiUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail";
-  
-    // Funzione per caricare i prodotti
+
     async function fetchProducts() {
         try {
             const response = await fetch(cocktailApiUrl);
             const data = await response.json();
             const products = data.drinks;
             renderProducts(products);
-  
-            // Nascondi il pulsante dopo aver caricato i prodotti
-            document.getElementById("showMoreButton").style.display = "none";
         } catch (error) {
             console.error("Errore nel caricamento dei prodotti", error);
         }
     }
-  
-    // Funzione per renderizzare i prodotti in righe da 4
+
     function renderProducts(products) {
-        const productList = document.querySelector('.container.my-4 .row'); // Trova la riga esistente
-  
-        // Suddividi i prodotti in gruppi di 4
-        for (let i = 0; i < products.length; i++) {
-            const product = products[i];
+        const productList = document.querySelector('.container.my-4 .row');
+
+        products.forEach(product => {
             const productDiv = document.createElement("div");
-            productDiv.classList.add("col-6", "col-md-3", "col-lg-2"); // Colonne per righe di 4 prodotti
-  
+            productDiv.classList.add("col-6", "col-md-3", "col-lg-2");
+
             productDiv.innerHTML = `
-                <div class="card text-center" style="background-color: #d5ab30;">
+                <div class="card text-center drink-card" style="background-color: #d5ab30; cursor: pointer;" data-id="${product.idDrink}">
                     <img class="card-img-top" src="${product.strDrinkThumb}" alt="${product.strDrink}">
                     <div class="card-body">
                         <h5 class="card-title">${product.strDrink}</h5>
                     </div>
                 </div>
             `;
-            productList.appendChild(productDiv);
-        }
-    }
-  
-    // Carica i prodotti
-    fetchProducts();
-    document.getElementById("showMoreButton").style.display = "none";
 
-  });
-  
+            productList.appendChild(productDiv);
+        });
+
+        // Aggiungo gli eventi di click DOPO che le card sono state create
+        document.querySelectorAll(".drink-card").forEach(card => {
+            card.addEventListener("click", (event) => {
+                const drinkId = event.currentTarget.getAttribute("data-id");
+                if (drinkId) {
+                    window.location.href = `drink.html?id=${drinkId}`;
+                }
+            });
+        });
+    }
+
+    fetchProducts();
+});
+
   
   
   
@@ -89,4 +90,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     fetchProducts();
 });
-
